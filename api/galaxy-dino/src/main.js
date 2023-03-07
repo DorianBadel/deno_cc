a122db0a40daea2b9cba0086a19d12f2715803fc
DorianBadel <dorian.badel@biss.hr>
2023-03-07 23:32:18 +0100

    Connection and FE

---
+++ api/galaxy-dino/src/main.js
@@ -0,0 +1,23 @@
+/// <reference lib="dom" />
+
+const getForm = document.querySelector("#get-form");
+const response = document.querySelector("#response");
+const status = document.querySelector("#status");
+
+getForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  try {
+    const formData = new FormData(getForm);
+    const slug = formData.get("slug");
+    const data = await fetch(`http://localhost:8000/${slug}`);
+    const json = await data.json();
+
+    if (data.ok) {
+      status.innerHTML = "Sucecess";
+      response.innerHTML = JSON.stringify(json, null, 2);
+    }
+  } catch (e) {
+    status.innerHTML = "Error";
+    response.innerHTML = JSON.stringify(e, null, 2);
+  }
+});
